7bd9bbc7a93b04ec13112885ff9b7dde402bf5f9
Bin WU <wubinben@icloud.com>
2020-09-01 22:17:22 +0800

    upgrade pact-jvm-consumer-junit to version 4.0.10

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# The Pact JVM Consumer Sample Project Based on pact-jvm-consumer-junit_2.11. 
+# The Pact JVM Consumer Sample Project 
 
-The repo "pact-ec-app" demonstrates an e-commerce application which depends on the "pact-ec-review-service" restful service. The former (as a consumer) requests the latter (as a provider) to provide rating information (such as a 3-star of a 5-star rating) for a product on the e-commerce website with product id and user name.
-
-The repo "pact-ec-app" is the sample project to show how to use the pact-jvm-consumer-junit_2.11. 
-
+The repo "pact-ec-app" demonstrates an e-commerce application 
+which depends on the "pact-ec-review-service" restful service. 
+The former (as a consumer) sends requests to the latter 
+(as a provider) to get rating information (5-star rating) 
+for a product on the e-commerce website with product id 
+and user name.
 
 # How to Run It
 0. Install jdk 1.8+ and maven 3
@@ -12,5 +14,3 @@ The repo "pact-ec-app" is the sample project to show how to use the pact-jvm-con
 3. The pact file target/pacts/ec_app-review_service.json will be generated.
 
 # Please refer to the ["pact-ec-review-service"](https://github.com/wubin28/pact-ec-review-service) for how to implement the corresponding pact provider.
-
-
--- src/test/java/kata/ReviewServiceProxyContractTest.java
+++ src/test/java/kata/ReviewServiceProxyContractTest.java
@@ -1,42 +1,48 @@
 package kata;
 
-import au.com.dius.pact.consumer.Pact;
-import au.com.dius.pact.consumer.PactProviderRuleMk2;
-import au.com.dius.pact.consumer.PactVerification;
+
 import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
-import au.com.dius.pact.model.PactSpecVersion;
-import au.com.dius.pact.model.RequestResponsePact;
-import org.jetbrains.annotations.NotNull;
+import au.com.dius.pact.consumer.junit.PactProviderRule;
+import au.com.dius.pact.consumer.junit.PactVerification;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
 import org.junit.Rule;
 import org.junit.Test;
 
+import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 public class ReviewServiceProxyContractTest {
-    @Rule
-    public PactProviderRuleMk2 rule = new PactProviderRuleMk2("review_service", "localhost", 8080, PactSpecVersion.V3, this);
 
+    public static final String PRODUCT_ID = "123456";
+    public static final String USER_NAME = "ben";
+    // This sets up a mock server that pretends to be our provider
+    @Rule
+    public PactProviderRule rule = new PactProviderRule("review_service", "localhost", 1234, this);
+
+    // This defines the expected interaction for out test
     @Pact(provider="review_service", consumer="ec_app")
-    public RequestResponsePact createPact(PactDslWithProvider builder) {
+    public RequestResponsePact pact(PactDslWithProvider builder) {
         // Pass the parameters to the provider by pact
         Map<String, Object> params = new HashMap<>();
-        params.put("productId", "123456");
-        params.put("userName", "ben");
+        params.put("productId", PRODUCT_ID);
+        params.put("userName", USER_NAME);
 
-        return builder.given("The ratings in Review service are ready", params)
-                .uponReceiving("A request for ratings for a product")
+        return builder
+                .given("The ratings in Review service are ready")
+                .uponReceiving("A request for ratings of a product")
                 .path("/ratings")
-                .query("productId=123456&userName=ben")
                 .method("GET")
+                .query("productId=" + PRODUCT_ID + "&" + "userName=" + USER_NAME)
                 .willRespondWith()
                 .headers(responseHeaders())
                 .status(200)
@@ -58,7 +64,7 @@ public class ReviewServiceProxyContractTest {
     @Test
     @PactVerification("review_service")
     public void should_get_a_list_of_ratings() {
-        ReviewServiceProxy reviewServiceProxy = new ReviewServiceProxy("http://localhost:8080/ratings?productId=123456&userName=ben");
+        ReviewServiceProxy reviewServiceProxy = new ReviewServiceProxy("http://localhost:1234/ratings?productId=123456&userName=ben");
 
         final List<Rating> actual = reviewServiceProxy.getRatings();
 
